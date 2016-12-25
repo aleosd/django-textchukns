d61b3d3bd4963eb1d7e0d6386c03c3f1d76b03ba
aleosd <osdalex@gmail.com>
2016-12-25 10:20:30 +0500

    Fix url in setup.py

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-textchunks',
-    version='0.1.0',
+    version='0.1.1',
     description='Keyed blocks of text or html content for use in your Django templates',
     keywords="django text chunks templates",
     author='Aleksey Osadchuk',
     author_email='osdalex@gmail.com',
-    url='https://github.com/gotlium/django-chunks2',
+    url='https://github.com/aleosd/django-textchukns',
     maintainer='aleosd',
     maintainer_email='osdalex@gmail.com',
     packages=find_packages(exclude=['demo']),
